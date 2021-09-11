1890e318910df81895ec4a7448507494b5d3f3e8
Tatii666 <tatianabezgina@icloud.com>
2021-09-11 18:58:46 +0300

    уроки 7-13. Сделана основа страницы и вынесены компоненты в отдельные файлы

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className = 'header' >
+        <img src= 'https://www.miraclenext.com/blog/wp-content/uploads/2013/04/unity_logo.jpg'/>
+    </header>
+}
+
+export default Header;
